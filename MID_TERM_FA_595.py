ad8b13667fdc1d43df7fea1fa15866b581a9e031
RiccardoPaladin <rpaladin@stevens.edu>
2022-03-10 13:17:16 -0500

    First commit

--- MID_TERM_FA_595.py
+++ MID_TERM_FA_595.py
@@ -15,3 +15,38 @@ import requests
 import streamlit as st
 
 st.title('NLP services')
+st.text('In this web app you can use six different services on NLP')
+
+st.text('FIRST SERVICE')
+st.text('Insert a string and obtain which part of the speech is')
+#Tokenize the sentence and return the pos_tag
+text_input = st.text_input('Enter string to analyze','')
+
+import nltk
+nltk.download('punkt')
+tokens = nltk.word_tokenize(text_input)
+tag = nltk.pos_tag(tokens)
+st.markdown(
+    f"""
+    {tag}
+    """
+)
+
+st.text('SECOND SERVICE')
+st.text('Insert a string and obtain the sentiment analysis')
+text_input1 = st.text_input('Enter string to evaluate','')
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+sia = SentimentIntensityAnalyzer()
+sentiment = sia.polarity_scores(text_input1)
+st.markdown(
+    f"""
+    {sentiment}
+    """
+)
+
+#streamlit run MID_TERM_FA_595    on the terminal
+#Local URL: http://localhost:8501
+#Network URL: http://192.168.12.117:8501
+
