a994a23e2064fd1b388b19c4cc28b4c2c35ff7a6
RiccardoPaladin <rpaladin@stevens.edu>
2022-03-10 11:19:25 -0500

    Sentiment Analysis for 100 companies

---
+++ MID_TERM_FA_595.py
@@ -0,0 +1,17 @@
+'''
+MID-TERM EXAM
+FA 595
+'''
+#https://docs.streamlit.io/library/get-started/create-an-app
+#AWS Amplify hosting --> Amplify Hosting is a fully managed hosting service for web apps. Connect your repository to build, deploy, and host your web app.
+
+
+
+#pip install streamlit
+
+import pandas as pd
+import numpy as np
+import requests
+import streamlit as st
+
+st.title('NLP services')
